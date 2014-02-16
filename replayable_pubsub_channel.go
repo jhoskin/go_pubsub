49a76c0b940b78aa6c5683260ecdd12474e6d281
Joh Hoskin <joh@dev.geek.nz>
2014-02-16 15:05:03 +1100

    playing with pointers to try and understand why I can't hold a reference to subscribers

--- replayable_pubsub_channel.go
+++ replayable_pubsub_channel.go
@@ -3,31 +3,44 @@ package replayable_pubsub_channel
 import (
 	"errors"
 	"container/list"
+	"log"
+	//"strconv"
 )
 
 type Replayable_pubsub_channel struct
 {
-	subscribers list.List
+	Subscribers list.List
 }
 
 func (c Replayable_pubsub_channel) lazyInit() {
-	//if c.subscribers == nil {
-	//	c.subscribers = list.New()
-	//}	
+	log.Printf("LazyInit")
+	if &c.Subscribers == nil {
+		log.Printf("creating new subscribers list")
+		c.Subscribers = *list.New()
+	}	
 }
 
 func (c Replayable_pubsub_channel) Publish(message string) (err error) {
+	log.Print("Called Publish")
 	c.lazyInit()
-	for e := c.subscribers.Front(); e != nil; e.Next() {
+	log.Printf("number of subscribers to publish to = %v", c.Subscribers.Len())
+	for e := c.Subscribers.Front(); e != nil; e.Next() {
+		log.Printf("sending message")
 		e.Value.(chan string) <- message
 	}	
 	return
 }
 
-func (c Replayable_pubsub_channel) Subscribe(receiver chan string) (err error) {
+func (c Replayable_pubsub_channel) Subscribe(receiver chan string, subs list.List) (s list.List, err error) {
+	log.Print("Called Subscribe")
+	log.Printf("lists are equal 1 = %v", &subs == &c.Subscribers)
+	log.Printf("lists are equal 1 = %v", &subs == &c.Subscribers)
 	c.lazyInit()
-	c.subscribers.PushBack(receiver)
-	return
+	log.Printf("lists are equal 2 = %v", &subs == &c.Subscribers)
+	c.Subscribers.PushBack(receiver)
+	log.Printf("lists are equal 3 = %v", &subs == &c.Subscribers)
+	log.Printf("number of subscribers = %v", c.Subscribers.Len())
+	return c.Subscribers, nil
 }
 
 func (c Replayable_pubsub_channel) Unsubscribe(receiver chan string) (err error) {
--- replayable_pubsub_channel_test.go
+++ replayable_pubsub_channel_test.go
@@ -2,69 +2,99 @@ package replayable_pubsub_channel
 
 import (
 	"testing"
-	"time"
+	//"time"
 	"container/list"
 )
+
+// func TestSubscribe2(t *testing.T) {
+// 	subscribers := list.New()
+
+// 	if subscribers.Len() != 0 {
+// 		t.Fatal("should be zero subscribers")
+// 	}
+
+// 	receiver := make(chan string, 100)
+	
+// 	subscribers.PushBack(receiver)
+
+// 	if subscribers.Len() != 1 {
+// 		t.Fatal("should be one subscriber")
+// 	}
+
+// 	if subscribers.Front().Value.(chan string) != receiver {
+// 		t.Fatal("receiver should be subscribed")
+// 	}
+// }
 
 func TestSubscribe(t *testing.T) {
 	subscribers := list.New()
 
 	receiver := make(chan string, 100)
-	c := Replayable_pubsub_channel{subscribers}
-	c.Subscribe(receiver)
+	c := Replayable_pubsub_channel{ *subscribers }
 
-	err := c.Subscribe(receiver)
+	if *subscribers != c.Subscribers {
+		t.Fatalf("lists not equal1")
+	}
+	
+	subs, err := c.Subscribe(receiver, *subscribers)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if subscribers.Len() != 1 {
-		t.Fatal("should be one subscriber")
+	t.Logf("number of subs = %v", subs.Len())
+	t.Logf("number of subs2 = %v", c.Subscribers.Len())
+
+	if subs != c.Subscribers {
+		t.Fatalf("lists not equal2")
 	}
 
-	if subscribers.Front().(chan string) != receiver {
+	if subscribers.Len() != 1 {
+		t.Fatalf("should be one subscriber, found %v", subscribers.Len())
+	}
+
+	if subscribers.Front().Value.(chan string) != receiver {
 		t.Fatal("receiver should be subscribed")
 	}
 }
 
-func TestPublish(t *testing.T) {
-	c := Replayable_pubsub_channel{}
-	err := c.Publish("test")
-	if err != nil {
-		t.Fatal(err)
-	}
-}
+// func TestPublish(t *testing.T) {
+// 	c := Replayable_pubsub_channel{}
+// 	err := c.Publish("test")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
 
-func TestEndToEnd(t *testing.T) {
-	subscribers := list.New()
+// func TestEndToEnd(t *testing.T) {
+// 	subscribers := list.New()
 
-	receiver := make(chan string, 100)
-	c := Replayable_pubsub_channel{subscribers}
-	c.Subscribe(receiver)
+// 	receiver := make(chan string, 100)
+// 	c := Replayable_pubsub_channel{ *subscribers }
+// 	c.Subscribe(receiver)
 
-	err := c.Subscribe(receiver)
-	if err != nil {
-		t.Error(err)
-	}
+// 	err := c.Subscribe(receiver)
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
 
-	err = c.Publish("test")
-	if err != nil {
-		t.Error(err)
-	}
+// 	err = c.Publish("test")
+// 	if err != nil {
+// 		t.Error(err)
+// 	}
 
-	timeout := make(chan bool, 1)
-	go func() {
-	    time.Sleep(1 * time.Second)
-	    timeout <- true
-	}()
+// 	timeout := make(chan bool, 1)
+// 	go func() {
+// 	    time.Sleep(1 * time.Second)
+// 	    timeout <- true
+// 	}()
 
-	var result string
-	select {
-		case result = <-receiver:
-			if result != "test" {
-				t.Errorf("wrong message received %v", result)
-			}
-		case <-timeout:
-			t.Errorf("timed out waiting for message")
-	}	
-}
+// 	var result string
+// 	select {
+// 		case result = <-receiver:
+// 			if result != "test" {
+// 				t.Errorf("wrong message received %v", result)
+// 			}
+// 		case <-timeout:
+// 			t.Errorf("timed out waiting for message")
+// 	}	
+// }
